a57452ac93dcb3d554e2f443a2b88bbaa3fddc5d
Vishal <vishal.yadav@sdempire.co.in>
2025-09-23 19:00:20 +0530

    fruits

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -55,7 +55,7 @@ export default function HomePage() {
                     <Link href="/fruits-options">
                         <div className="relative group overflow-hidden rounded-lg shadow-md cursor-pointer">
                             <img
-                                src="/images/veg.jpg"
+                                src="/images/fr.jpg"
                                 alt="Fruits"
                                 className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-105"
                             />
