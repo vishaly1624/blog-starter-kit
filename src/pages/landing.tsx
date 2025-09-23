ba680b99c44fcfe5847da25437f8390bbe027510
Vishal <vishal.yadav@sdempire.co.in>
2025-09-23 18:45:56 +0530

    fruit

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -56,7 +56,7 @@ export default function HomePage() {
                         <div className="relative group overflow-hidden rounded-lg shadow-md cursor-pointer">
                             <img
                                 src="/images/veg.jpg"
-                                alt="Vegetables"
+                                alt="Fruits"
                                 className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-105"
                             />
                             <div className="absolute inset-0 bg-green-900/40 flex flex-col justify-end p-4 text-white">
